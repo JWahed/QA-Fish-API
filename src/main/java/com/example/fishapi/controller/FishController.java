30fe2ff1da7a526745afed8effd8a3145c5b95f6
JWahed <jak.clev@gmail.com>
2022-08-10 10:42:34 +0100

    Update FishController.java
    removed unused imports

--- src/main/java/com/example/fishapi/controller/FishController.java
+++ src/main/java/com/example/fishapi/controller/FishController.java
@@ -2,13 +2,12 @@ package com.example.fishapi.controller;
 
 import com.example.fishapi.entity.Fish;
 import com.example.fishapi.service.FishService;
-import org.apache.coyote.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
