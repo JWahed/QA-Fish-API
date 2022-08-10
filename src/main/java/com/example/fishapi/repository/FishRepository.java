c559c7446a7202b3cd2c5e04370c9903289f9653
JWahed <jak.clev@gmail.com>
2022-08-10 10:26:28 +0100

    Update FishRepository.java
    created custom queries

--- src/main/java/com/example/fishapi/repository/FishRepository.java
+++ src/main/java/com/example/fishapi/repository/FishRepository.java
@@ -2,9 +2,17 @@ package com.example.fishapi.repository;
 
 import com.example.fishapi.entity.Fish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FishRepository extends JpaRepository<Fish, Long> {
 
+    List<Fish> findFishByName(String name);
+    List<Fish> findAllFishByPrice(double price);
+
+    @Query("Select f from Fish f WHERE f.quantity = ?1 and f.price = ?2")
+    List<Fish> findAllFishByQuantityAndPrice(int quantity, double price);
 }
