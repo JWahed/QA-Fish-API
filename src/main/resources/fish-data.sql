ed48e5b9f8631d3406246e11f69182c0a0c7cd9c
JWahed <jak.clev@gmail.com>
2022-08-10 16:33:37 +0100

    Create fish-data.sql

---
+++ src/main/resources/fish-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `fish`(`id`, `name`, `date_caught`, `price`, `quantity`)
+VALUES
+(default, `Salmon`, `2022-8-1`, 13, 6.99),
+(default, `Haddock`, `2022-7-30`, 36, 3.50),
+(default, `Mackerel`, `2022-8-8`, 55, 3.6),
+(default, `Cod`, `2022-7-27`, 24, 4.25),
+(default, `Sea bass`, `2022-8-4`, 43, 3.85);
