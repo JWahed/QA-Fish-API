d973fbc2fb4bcaf11ccb08bfab747008c5179add
JWahed <jak.clev@gmail.com>
2022-08-11 09:03:53 +0100

    Update fish-schema.sql
    changed order of last two columns

--- src/main/resources/fish-schema.sql
+++ src/main/resources/fish-schema.sql
@@ -4,6 +4,6 @@ CREATE TABLE `fish` (
         `id`          BIGINT(255) PRIMARY KEY AUTO_INCREMENT,
         `name`        VARCHAR(255),
         `date_caught` DATE,
-        `price`       DOUBLE PRECISION,
-        `quantity`    INTEGER
+        `quantity`    INTEGER,
+        `price`       DOUBLE PRECISION
 );
