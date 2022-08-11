a45e2ae2409ebdb07c9ae0cfe7535443e91b3e67
JWahed <jak.clev@gmail.com>
2022-08-11 09:04:57 +0100

    Update fish-data.sql
    changed order of last two columns on line 1 to match change in fish-schema.sql

--- src/main/resources/fish-data.sql
+++ src/main/resources/fish-data.sql
@@ -1,7 +1,7 @@
-INSERT INTO `fish`(`id`, `name`, `date_caught`, `price`, `quantity`)
+INSERT INTO `fish` (`id`, `name`, `date_caught`, `quantity`, `price`)
 VALUES
-(default, `Salmon`, `2022-8-1`, 13, 6.99),
-(default, `Haddock`, `2022-7-30`, 36, 3.50),
-(default, `Mackerel`, `2022-8-8`, 55, 3.6),
-(default, `Cod`, `2022-7-27`, 24, 4.25),
-(default, `Sea bass`, `2022-8-4`, 43, 3.85);
+    (default, "salmon", "2022-08-01", 13, 6.99),
+    (default, "haddock", "2022-07-30", 36, 3.50),
+    (default, "mackerel", "2022-08-08", 55, 3.6),
+    (default, "cod", "2022-07-27", 24, 4.25),
+    (default, "sea bass", "2022-08-04", 43, 3.85);
